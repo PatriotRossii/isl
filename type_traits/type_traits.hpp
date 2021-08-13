ff9d692441a206211f1c0e98168b6f8fc39c428d
PatriotRossii <patriotrossii2019@mail.ru>
2021-08-13 07:16:27 +0000

    Little renaiming, implement isl::remove_cv, isl::remove_volatile, isl::remove_constant

---
+++ type_traits/type_traits.hpp
@@ -0,0 +1,112 @@
+// Helper classes
+namespace isl {
+    template<class T, T v> struct integral_constant {
+        static constexpr T value = v;
+
+        using value_type = T;
+        using type = integral_constant;
+
+        constexpr operator value_type() const noexcept {
+            return value;
+        }
+
+        constexpr value_type operator()() const noexcept {
+            return value;
+        }
+    };
+    template <bool B>
+    using bool_constant = integral_constant<bool, B>;
+
+    using true_type = integral_constant<bool, true>;
+    using false_type = integral_constant<bool, false>;
+}
+
+// Const-volatility specifiers
+namespace isl {
+    // remove_const
+
+    template<class T>
+    struct remove_const {
+        using type = T;
+    };
+    template<class T>
+    struct remove_const<T*> {
+        using type = T*;
+
+    };
+
+    template<class T>
+    struct remove_const<const T> {
+        using type = T;
+    };
+    template<class T>
+    struct remove_const<T* const> {
+        using type = T*;
+    };
+
+    template<class T>
+    using remove_const_t = typename remove_const<T>::type;
+
+    // remove_volatile
+
+    template<class T>
+    struct remove_volatile {
+        using type = T;
+    };
+    template<class T>
+    struct remove_volatile<T*> {
+        using type = T*;
+    };
+
+    template<class T>
+    struct remove_volatile<volatile T> {
+        using type = T;
+    };
+    template<class T>
+    struct remove_volatile<T* volatile> {
+        using type = T*;
+    };
+
+    template<class T>
+    using remove_volatile_t = typename remove_volatile<T>::type;
+
+    // remove_cv
+
+    template<class T>
+    struct remove_cv {
+        using type = T;
+    };
+    template<class T>
+    struct remove_cv<T*> {
+        using type = T*;
+    };
+
+    template<class T>
+    struct remove_cv<const volatile T> {
+        using type = T;
+    };
+    template<typename T>
+    struct remove_cv<volatile T> {
+        using type = T;
+    };
+    template<typename T>
+    struct remove_cv<const T> {
+        using type = T;
+    };
+    template<typename T>
+    struct remove_cv<T* volatile> {
+        using type = T*;
+    };
+    template<typename T>
+    struct remove_cv<T* const> {
+        using type = T*;
+    };
+    template<typename T>
+    struct remove_cv<T* const volatile> {
+        using type = T*;
+    };
+
+    template<class T>
+    using remove_cv_t = typename remove_cv<T>::type;
+}
+
