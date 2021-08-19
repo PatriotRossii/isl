ff7fdf7c464b522be8249c5d9599630659d0485c
PatriotRossii <patriotrossii2019@mail.ru>
2021-08-19 11:19:21 +0500

    [named_requirements] [basic] Rewrite MoveAssignable, CopyAssignable concepts in correct way

--- internal/named_requirements/basic.hpp
+++ internal/named_requirements/basic.hpp
@@ -45,43 +45,40 @@ namespace isl::internal {
 
 	namespace detail {
 		template<typename T>
-		bool test_move_assignable() {
-			static_assert(
-				isl::is_same_v<
-					decltype(static_cast<T&>(
-						isl::declval<T>()
-					) = isl::declval<T>()),
-				T&>
-			);
-		}
+		auto test_move_assignable(int) -> isl::conditional_t<
+			isl::is_same_v<
+				decltype(
+					static_cast<T&>(isl::declval<T>()) = isl::declval<T>()
+				),
+				T&
+			> == true, bool, void
+		>;
 		template<typename T>
-		void test_move_assignable();
+		auto test_move_assignable(...) -> void;
 	}
 
 	template<typename T>
 	concept MoveAssignable = isl::is_same_v<
-		decltype(test_move_assignable<T>()), bool
+		decltype(test_move_assignable<T>(0)), bool
 	>;
 
 	namespace detail {
 		template<typename T>
-		bool test_copy_assignable() {
-			static_assert(
-				isl::is_same_v<
-					decltype(
-						static_cast<T&>(isl::declval<T>()) = static_cast<const T&>(isl::declval<T>())
-					),
-					T&
-				>
-			);
-		}
+		auto test_copy_assignable(int) -> isl::conditional_t<
+			isl::is_same_v<
+				decltype(
+					static_cast<T&>(isl::declval<T>()) = static_cast<const T&>(isl::declval<T>())
+				),
+				T&
+			> == true, bool, void
+		>;
 		template<typename T>
-		void test_copy_assignable();
+		auto test_copy_assignable(...) -> void;
 	}
 
 	template<typename T>
 	concept CopyAssignable = isl::is_same_v<
-		decltype(test_copy_assignable<T>()), bool
+		decltype(test_copy_assignable<T>(0)), bool
 	>;
 
 	template<typename T>
