11e69ff4ba9387879b98c5fe4215f4c49846d935
PatriotRossii <patriotrossii2019@mail.ru>
2021-08-19 09:40:16 +0500

    [named_requirements] Get rid of duplicated is_same trait, include utility.hpp

--- internal/named_requirements/basic.hpp
+++ internal/named_requirements/basic.hpp
@@ -1,18 +1,6 @@
+#include "../../utility/utility.hpp"
+
 namespace isl::internal {
-	template<typename T, typename U>
-	struct is_same {
-		constexpr static bool value = false;
-	};
-
-	template<typename T>
-	struct is_same<T, T> {
-		constexpr static bool value = true;
-	};
-
-
-	template<typename T, typename U>
-	constexpr bool is_same_v = isl::internal::is_same<T, U>::value;
-
 	namespace detail {
 		template<typename T>
 		bool test_default_constructible() {
@@ -32,7 +20,7 @@ namespace isl::internal {
 	}
 
 	template<typename T>
-	concept DefaultConstructible = is_same_v<
+	concept DefaultConstructible = isl::is_same_v<
 			decltype(test_default_constructible<T>()), bool
 	>;
 }
