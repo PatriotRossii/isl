da7032fff700c5e1865f99366abe296a8f7d9c58
PatriotRossii <patriotrossii2019@mail.ru>
2021-08-30 13:08:31 +0000

    [vector] Implement isl::vector member types

--- src/vector/vector.cpp
+++ src/vector/vector.cpp
@@ -1,15 +1,43 @@
 module;
 
 #include <memory> // std::allocator
+#include <cstdint> // std::size_t
+#include <iterator> // std::reverse_iterator
 
 export module vector;
+
+namespace std {
+	namespace pmr {
+		template<class T = std::byte>
+		class polymorphic_allocator;
+	}
+}
 
 export namespace isl {
 	template<class T,
 			class Allocator = std::allocator<T>
-	> class vector;
+	> class vector {
+		using value_type = T;
+		using allocator_type = Allocator;
+
+		using size_type = std::size_t;
+		using difference_type = std::ptrdiff_t;
+
+		using reference = value_type&;
+		using const_reference = const value_type&;
+
+		using pointer = typename std::allocator_traits<Allocator>::pointer;
+		using const_pointer = typename std::allocator_traits<Allocator>::const_pointer;
+
+		using iterator = T*;
+		using const_iterator = const T*;
+
+		using reverse_iterator = std::reverse_iterator<iterator>;
+		using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+	};
 	namespace pmr {
 		template<class T>
-		using vector = std::vector<T, std::pmr::polymorphic_allocator<T>>;
+		using vector = isl::vector<T, std::pmr::polymorphic_allocator<T>>;
 	}
 }
+
