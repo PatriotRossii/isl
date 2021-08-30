ca336698d2a7f5ff1807d1b212d1fe205da639eb
PatriotRossii <patriotrossii2019@mail.ru>
2021-08-30 13:38:21 +0000

    [vector] Implement constructors

--- src/vector/vector.cpp
+++ src/vector/vector.cpp
@@ -4,6 +4,9 @@ module;
 #include <cstdint> // std::size_t
 #include <iterator> // std::reverse_iterator
 
+#include <initializer_list> // std::initializer_list
+#include <algorithm> // std::uninitalized_fill_n
+
 export module vector;
 
 namespace std {
@@ -17,6 +20,10 @@ export namespace isl {
 	template<class T,
 			class Allocator = std::allocator<T>
 	> class vector {
+	private:
+		Allocator allocator;
+		T* storage;
+	public:
 		using value_type = T;
 		using allocator_type = Allocator;
 
@@ -34,6 +41,72 @@ export namespace isl {
 
 		using reverse_iterator = std::reverse_iterator<iterator>;
 		using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+		constexpr vector() noexcept(noexcept(Allocator())) { }
+		constexpr explicit vector(const Allocator& alloc) noexcept: allocator(alloc) { }
+		constexpr vector(size_type count,
+						 const T& value,
+						 const Allocator& alloc = Allocator()): allocator(alloc) {
+			this->storage = allocator.allocate(count);
+			std::uninitialized_fill_n(
+				storage, count, value
+			);
+		}
+		constexpr explicit vector(size_type count,
+								  const Allocator& alloc = Allocator()): allocator(alloc) {
+			this->storage = allocator.allocate(count);
+			std::uninitialized_default_construct_n(
+				storage, count
+			);
+		}
+		template<class InputIt>
+		constexpr vector(InputIt first, InputIt last,
+						 const Allocator& alloc = Allocator()): allocator(alloc) {
+			this->storage = allocator.allocate(last - first);
+			std::copy(
+				first, last, this->begin()
+			);
+		}
+		constexpr vector(const vector& other) {
+			size_t other_size = other.size();
+
+			this->storage = std::allocator_traits<allocator_type>::select_on_container_copy_construction(
+    other.get_allocator()).allocate(other_size);
+			std::copy_n(
+				other.begin(), other_size, this->storage
+			);
+		}
+		constexpr vector(const vector& other, const Allocator& alloc): allocator(alloc) {
+			size_t other_size = other.size();
+
+			this->storage = allocator.allocate(other_size);
+			std::copy_n(
+				other.begin(), other_size, this->storage
+			);
+		}
+		constexpr vector(vector&& other) noexcept {
+			size_t other_size = other.size();
+
+			this->storage = allocator.allocate(other_size);
+			std::move(
+				other.storage, other.storage + other_size, this->storage
+			);
+		}
+		constexpr vector(vector&& other, const Allocator& alloc): allocator(alloc) {
+			size_t other_size = other.size();
+
+			this->storage = allocator.allocate(other_size);
+			std::move(
+				other.storage, other.storage + other_size, this->storage
+			);
+		}
+		constexpr vector(std::initializer_list<T> init,
+						 const Allocator& alloc = Allocator()): allocator(alloc) {
+			this->storage = allocator.allocate(init.size());
+			std::copy_n(
+				init.begin(), init.end(), storage
+			);
+		}
 	};
 	namespace pmr {
 		template<class T>
