8de8d20e3d995f666bea374a0a5705f7a72e0ed4
PatriotRossii <patriotrossii2019@mail.ru>
2022-04-14 17:17:51 +0500

    Implement ratio comparison methods

--- ratio/ratio.hpp
+++ ratio/ratio.hpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include <numeric>
 #include <cmath>
+#include <type_traits>
 
 namespace isl {
     template<
@@ -13,43 +14,85 @@ namespace isl {
     public:
         static constexpr std::intmax_t num = std::signbit(Denom) * Num / std::gcd(Num, Denom);
         static constexpr std::intmax_t den = std::abs(Denom) / std::gcd(Num, Denom);
-        using type = isl::ratio<num, den>;
+        using type = ratio<num, den>;
     };
 
     template<class R1, class R2>
-    using ratio_add = typename isl::ratio<
+    using ratio_add = typename ratio<
         R1::num * R2::den + R2::num * R1::den, R1::den * R2::den
     >::type;
 
     template<class R1, class R2>
-    using ratio_subtract = typename isl::ratio<
+    using ratio_subtract = typename ratio<
         R1::num * R2::den - R2::num * R1::den, R1::den * R2::den
     >::type;
 
     template<class R1, class R2>
-    using ratio_multiply = typename isl::ratio<
+    using ratio_multiply = typename ratio<
         R1::num * R2::num, R1::den * R2::den
     >::type;
 
     template<class R1, class R2>
-    using ratio_divide = typename isl::ratio<
+    using ratio_divide = typename ratio<
         R1::num * R2::den, R1::den * R2::num
     >::type;
 
-    using atto = isl::ratio<1, 1000000000000000000>;
-    using femto = isl::ratio<1, 1000000000000000>;
-    using pico = isl::ratio<1, 1000000000000>;
-    using nano = isl::ratio<1, 1000000000>;
-    using micro = isl::ratio<1, 1000000>;
-    using milli = isl::ratio<1, 1000>;
-    using centi = isl::ratio<1, 100>;
-    using deci = isl::ratio<1, 10>;
-    using deca = isl::ratio<10, 1>;
-    using hecto = isl::ratio<100, 1>;
-    using kilo = isl::ratio<1000, 1>;
-    using mega = isl::ratio<1000000, 1>;
-    using giga = isl::ratio<1000000000, 1>;
-    using tera = isl::ratio<1000000000000, 1>;
-    using peta = isl::ratio<1000000000000000, 1>;
-    using exa = isl::ratio<1000000000000000000, 1>;
+    template<class R1, class R2>
+    struct ratio_equal: std::integral_constant<
+        bool, R1::num == R2::num && R1::den == R2::den
+    > { };
+    template<class R1, class R2>
+    inline constexpr bool ratio_equal_v = ratio_equal<R1, R2>::value;
+
+    template<class R1, class R2>
+    struct ratio_not_equal: std::integral_constant<
+        bool, !ratio_equal_v<R1, R2>
+    > { };
+    template<class R1, class R2>
+    inline constexpr bool ratio_not_equal_v = ratio_not_equal<R1, R2>::value;
+
+    template<class R1, class R2>
+    struct ratio_less: std::integral_constant<
+        bool, R1::num * R2::den < R2::num * R1::den
+    > { };
+    template<class R1, class R2>
+    inline constexpr bool ratio_less_v = ratio_less<R1, R2>::value;
+
+    template<class R1, class R2>
+    struct ratio_less_equal: std::integral_constant<
+        bool, ratio_less_v<R1, R2> || ratio_equal_v<R1, R2>
+    > { };
+    template<class R1, class R2>
+    inline constexpr bool ratio_less_equal_v = ratio_less_equal<R1, R2>::value;
+
+    template<class R1, class R2>
+    struct ratio_greater: std::integral_constant<
+        bool, (R1::num * R2::den > R2::num * R1::den)
+    > { };
+    template<class R1, class R2>
+    inline constexpr bool ratio_greater_v = ratio_greater<R1, R2>::value;
+
+    template<class R1, class R2>
+    struct ratio_greater_equal: std::integral_constant<
+        bool, ratio_greater_v<R1, R2> || ratio_equal_v<R1, R2>
+    > { };
+    template<class R1, class R2>
+    inline constexpr bool ratio_greater_equal_v = ratio_greater<R1, R2>::value;
+
+    using atto = ratio<1, 1000000000000000000>;
+    using femto = ratio<1, 1000000000000000>;
+    using pico = ratio<1, 1000000000000>;
+    using nano = ratio<1, 1000000000>;
+    using micro = ratio<1, 1000000>;
+    using milli = ratio<1, 1000>;
+    using centi = ratio<1, 100>;
+    using deci = ratio<1, 10>;
+    using deca = ratio<10, 1>;
+    using hecto = ratio<100, 1>;
+    using kilo = ratio<1000, 1>;
+    using mega = ratio<1000000, 1>;
+    using giga = ratio<1000000000, 1>;
+    using tera = ratio<1000000000000, 1>;
+    using peta = ratio<1000000000000000, 1>;
+    using exa = ratio<1000000000000000000, 1>;
 }
